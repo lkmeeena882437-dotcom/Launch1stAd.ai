752ea0259c1b88abec1abc5f4f450f19acab45eb
lucky <lkmeeena882437@gmail.com>
2026-06-21 13:48:48 +0530

    Add app layout

---
+++ app/layout.tsx
@@ -0,0 +1,9 @@
+import "./globals.css";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className="font-sans antialiased">{children}</body>
+    </html>
+  );
+}
